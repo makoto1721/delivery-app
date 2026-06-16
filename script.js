87c0a3d4a0eb6374b3e9e134f8b240ac6c306257
makoto1721 <mykrn0917@gmail.com>
2026-06-16 10:20:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -486,7 +486,25 @@ deliveryPerHour:
     rocketCount:getNumber("rocketCount"),
     rocketSales:getNumber("rocketSales"),
 
-   memo:
+start1:
+  document.getElementById("start1").value,
+
+end1:
+  document.getElementById("end1").value,
+
+start2:
+  document.getElementById("start2").value,
+
+end2:
+  document.getElementById("end2").value,
+
+start3:
+  document.getElementById("start3").value,
+
+end3:
+  document.getElementById("end3").value,
+
+memo:
   document.getElementById("memo").value,
 
 isRainy:isRainy,
