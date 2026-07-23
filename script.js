ea8cd7445660caf7ecd1c674f76fc0d5b9dddfeb
makoto1721 <mykrn0917@gmail.com>
2026-07-23 13:15:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4732,7 +4732,18 @@ function toggleRecordBar(){
   if(!card) return;
 
 
-  card.classList.toggle("mini");
+  const isMini =
+    card.classList.contains("mini");
 
+
+  if(isMini){
+
+    card.classList.remove("mini");
+
+  }else{
+
+    card.classList.add("mini");
+
+  }
 
 }
